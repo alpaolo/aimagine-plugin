fa1e13c2f71adb2b733a7cc688a7338c3bce273e
alpaolo <paolo.alberti@clipart.it>
2021-03-21 12:46:16 +0100

    bootstrap

---
+++ data/articles.py
@@ -0,0 +1,31 @@
+products = [
+    {
+        'id': '1',
+        'nome': 'biscottone',
+        'note': 'Il sapore tipico della pastafrolla di una volta, la forma grande e rettangolare del biscotto artigianale e la doratura di una spolverata di zucchero di canna: sono questi gli ingredienti segreti per un biscotto che ti fa ritrovare ogni mattina i gusti semplici della tradizione.',
+        'id_produttore': '1'
+    },
+    {
+        'id': '2',
+        'nome': 'goloso',
+        'note':'',
+        'id_produttore': '4'
+    },
+    {
+        'id': '3',
+        'nome': 'carezze',
+        'note':'',
+        'id_produttore': '5'
+    },
+    {
+        'id': '4',
+        'nome': 'cecca',
+        'note':'',
+        'id_produttore': '6'
+    }
+]
+
+
+
+def get_products():
+    return products
---
+++ data/data.php
@@ -0,0 +1,369 @@
+<?php
+global $data_a;
+$data_a = ["ingredients" => 
+    
+    [[
+        "id"=> "1",
+        "nome"=> "farina 00",
+        "id_fornitore"=> "1",
+        "id_prodotto"=> "1",
+        "scheda"=>"",
+        "semilavorato"=> "False"
+    ],
+    [
+        "id"=> "2",
+        "nome"=> "uova",
+        "id_fornitore"=> "2",
+        "id_prodotto"=> "1",
+        "scheda"=>"",
+        "semilavorato"=> "False"
+    ],
+    [
+        "id"=> "3",
+        "nome"=> "farina 00",
+        "id_fornitore"=> "3",
+        "id_prodotto"=> "2",
+        "scheda"=>"",
+        "semilavorato"=> "False"
+    ],
+    [
+        "id"=> "4",
+        "nome"=> "zucchero",
+        "id_fornitore"=> "3",
+        "id_prodotto"=> "1",
+        "scheda"=>"",
+        "semilavorato"=> "False"
+    ],
+    [
+        "id"=> "5",
+        "nome"=> "uova",
+        "id_fornitore"=> "4",
+        "id_prodotto"=> "2",
+        "scheda"=>"",
+        "semilavorato"=> "False"
+    ],
+    [
+        "id"=> "7",
+        "nome"=> "farina 000",
+        "id_fornitore"=> "5",
+        "id_prodotto"=> "3",
+        "scheda"=>"",
+        "semilavorato"=> "False"
+    ],
+    [
+        "id"=> "8",
+        "nome"=> "acqua",
+        "id_fornitore"=> "",
+        "id_prodotto"=> "4",
+        "scheda"=>"",
+        "semilavorato"=> "False"
+    ],
+    [
+        "id"=> "9",
+        "nome"=> "zucchero",
+        "id_fornitore"=> "7",
+        "id_prodotto"=> "4",
+        "scheda"=>"",
+        "semilavorato"=> "False"
+    ],
+    [
+        "id"=> "10",
+        "nome"=> "tuorli",
+        "id_fornitore"=> "8",
+        "id_prodotto"=> "4",
+        "scheda"=>"https=>//www.gruppomoretti.com/brands/ovoprodotti-professional",
+        "semilavorato"=> "False"
+    ],
+    [
+        "id"=> "11",
+        "nome"=> "burro",
+        "id_fornitore"=> "9",
+        "id_prodotto"=> "4",
+        "scheda"=>"https=>//www.italbutter.it/burro-per-pasticcerie",
+        "semilavorato"=> "False"
+    ],
+    [
+        "id"=> "12",
+        "nome"=> "lievito madre",
+        "id_fornitore"=> "10",
+        "id_prodotto"=> "4",
+        "scheda"=>"",
+        "semilavorato"=> "False"
+    ],
+    [
+        "id"=> "13",
+        "nome"=> "farina",
+        "id_fornitore"=> "10",
+        "id_prodotto"=> "4",
+        "scheda"=>"https=>//www.farinapetra.it/SCHEDATEC/?permalink=petra-1119",
+        "semilavorato"=> "False"
+    ],
+    [
+        "id"=> "14",
+        "nome"=> "miele",
+        "id_fornitore"=> "11",
+        "id_prodotto"=> "4",
+        "scheda"=>"",
+        "semilavorato"=> "False"
+    ],
+    [
+        "id"=> "15",
+        "nome"=> "pasta arancia",
+        "id_fornitore"=> "12",
+        "id_prodotto"=> "4",
+        "scheda"=>"https=>//www.disiosrl.it/it/articoli?id=34&n=Mediterraneo",
+        "semilavorato"=> "True"
+    ],
+    [
+        "id"=> "16",
+        "nome"=> "panna",
+        "id_fornitore"=> "13",
+        "id_prodotto"=> "4",
+        "scheda"=>"https=>//www.frascheriprofessionale.it/prodotto/panna-esl-da-montare-per-pasticceria/",
+        "semilavorato"=> "False"
+    ],
+    [
+        "id"=> "17",
+        "nome"=> "sale",
+        "id_fornitore"=> "",
+        "id_prodotto"=> "4",
+        "scheda"=>"",
+        "semilavorato"=> "False"
+    ],
+    [
+        "id"=> "18",
+        "nome"=> "nocciole",
+        "id_fornitore"=> "14",
+        "id_prodotto"=> "4",
+        "scheda"=>"http=>//www.tondagentileprelibata.it/nocciuolo.htm",
+        "semilavorato"=> "False"
+    ],
+    [
+        "id"=> "19",
+        "nome"=> "mandorle",
+        "id_fornitore"=> "15",
+        "id_prodotto"=> "4",
+        "scheda"=>"https=>//www.manuzzisrl.it/catalogo/mandorle-sgusciate-e-pelate-y78",
+        "semilavorato"=> "False"
+    ],
+    [
+        "id"=> "20",
+        "nome"=> "armelline",
+        "id_fornitore"=> "",
+        "id_prodotto"=> "4",
+        "scheda"=>"",
+        "semilavorato"=> "False"
+    ],
+    [
+        "id"=> "21",
+        "nome"=> "farina di mais",
+        "id_fornitore"=> "10",
+        "id_prodotto"=> "4",
+        "scheda"=>"",
+        "semilavorato"=> "False"
+    ],
+    [
+        "id"=> "22",
+        "nome"=> "fecola di patate",
+        "id_fornitore"=> "",
+        "id_prodotto"=> "4",
+        "scheda"=>"",
+        "semilavorato"=> "False"
+    ],
+    [
+        "id"=> "23",
+        "nome"=> "albume",
+        "id_fornitore"=> "16",
+        "id_prodotto"=> "4",
+        "scheda"=>"https=>//www.gruppomoretti.com/brands/ovoprodotti-professional",
+        "semilavorato"=> "True"
+    ],
+    [
+        "id"=> "24",
+        "nome"=> "latte",
+        "id_fornitore"=> "17",
+        "id_prodotto"=> "4",
+        "scheda"=>"https=>//www.sterilgarda.it/latte-e-derivati/latte-uht-sterilgarda-parzialmente-scremato-1500ml/",
+        "semilavorato"=> "False"
+    ],
+    [
+        "id"=> "25",
+        "nome"=> "amido di riso",
+        "id_fornitore"=> "",
+        "id_prodotto"=> "4",
+        "scheda"=>"",
+        "semilavorato"=> "False"
+    ],
+    [
+        "id"=> "26",
+        "nome"=> "amarena candita",
+        "id_fornitore"=> "18",
+        "id_prodotto"=> "4",
+        "scheda"=>"https=>//www.cesarin.it/ita/content/dolcefrutta-professional",
+        "semilavorato"=> "True"
+    ]
+    ],
+    "producers" => [
+        [
+            "id"=> "1",
+            "nome"=> "Barilla",
+            "sito"=> "http=>\/\/www.barilla.it",
+            "note"=>"null"
+        ],
+        [
+            "id"=> "4",
+            "nome"=> "Bistefani",
+            "sito"=> "http=>\/\/www.bistefani.it",
+            "note"=>"null"
+        ],
+        [
+            "id"=> "5",
+            "nome"=> "Colussi",
+            "sito"=> "http=>\/\/www.colussi.it",
+            "note"=>"null"
+        ],
+        [
+            "id"=> "6",
+            "nome"=> "Pasticceria Cucchi",
+            "sito"=> "http=>\/\/www.cucchi.it",
+            "note"=>"null"
+        ]
+    ],
+    "suppliers" => [
+        [
+          "id"=> "1",
+          "nome"=> "Agraria Tornabene Srl",
+          "sito"=> "http=>//www.tornabene.it",
+          "origine"=> "IT"
+        ],
+        [
+          "id"=> "2",
+          "nome"=> "Allevamento Galline Spa",
+          "sito"=> "http=>//www.algall.it",
+          "origine"=> "IT"
+        ],
+        [
+          "id"=> "3",
+          "nome"=> "Az. Agricola Ladispoli",
+          "sito"=> "http=>//www.ladispoli.it",
+          "origine"=> "IT"
+        ],
+        [
+          "id"=> "4",
+          "nome"=> "Uova&Uova",
+          "sito"=> "http=>//www.biuova.it",
+          "origine"=> "IT"
+        ],
+        [
+          "id"=> "5",
+          "nome"=> "Zuccherificio Pasticcio",
+          "sito"=> "http=>//www.pasticcio.it",
+          "origine"=> "IT"
+        ],
+         [
+          "id"=> "6",
+          "nome"=> "Zuccherificio Pasticcio",
+          "sito"=> "http=>//www.pasticcio.it",
+          "origine"=> "IT"
+        ],
+         [
+          "id"=> "7",
+          "nome"=> "Italia Zuccheri",
+          "sito"=> "https=>//www.italiazuccheri.it/chi-siamo/",
+          "origine"=> "IT"
+        ],
+         [
+          "id"=> "8",
+          "nome"=> "Gruppo Moretti",
+          "sito"=> "https=>//www.gruppomoretti.com/home",
+          "origine"=> "IT"
+        ],
+         [
+          "id"=> "9",
+          "nome"=> "Ital Butter",
+          "sito"=> "https=>//www.italbutter.it/azienda",
+          "origine"=> "IT"
+        ],
+        [
+          "id"=> "10",
+          "nome"=> "Farina Petra",
+          "sito"=> "https=>//www.farinapetra.it/chisiamo/",
+          "origine"=> "IT"
+        ],
+        [
+          "id"=> "11",
+          "nome"=> "Azienda agricola Ballari",
+          "sito"=> "https=>//www.antichebonta.com/azienda.html",
+          "origine"=> "IT"
+        ],
+        [
+          "id"=> "12",
+          "nome"=> "Di sio srl",
+          "sito"=> "https=>//www.disiosrl.it/it/azienda",
+          "origine"=> "IT"
+        ],
+        [
+          "id"=> "13",
+          "nome"=> "Frascheri",
+          "sito"=> "https=>//www.disiosrl.it/it/azienda",
+          "origine"=> "IT"
+        ],
+        [
+          "id"=> "14",
+          "nome"=> "tondagentileprelibata",
+          "sito"=> "http=>//www.tondagentileprelibata.it/",
+          "origine"=> "IT"
+        ],
+        [
+          "id"=> "15",
+          "nome"=> "Manuzzi",
+          "sito"=> "https=>//www.manuzzisrl.it/",
+          "origine"=> "IT"
+        ],
+        [
+          "id"=> "16",
+          "nome"=> "Gruppo Moretti",
+          "sito"=> "https=>//www.gruppomoretti.com/",
+          "origine"=> "IT"
+        ],
+        [
+          "id"=> "17",
+          "nome"=> "Sterilgarda",
+          "sito"=> "https=>//www.sterilgarda.it",
+          "origine"=> "IT"
+        ],
+        [
+          "id"=> "18",
+          "nome"=> "Cesarin",
+          "sito"=> "https=>//www.cesarin.it/ita",
+          "origine"=> "IT"
+        ]
+      ],
+      "products" => [
+        [
+            "id"=> "1",
+            "nome"=> "biscottone",
+            "note"=> "Il sapore tipico della pastafrolla di una volta, la forma grande e rettangolare del biscotto artigianale e la doratura di una spolverata di zucchero di canna=> sono questi gli ingredienti segreti per un biscotto che ti fa ritrovare ogni mattina i gusti semplici della tradizione.",
+            "id_produttore"=> "1"
+        ],
+        [
+            "id"=> "2",
+            "nome"=> "goloso",
+            "note"=>"",
+            "id_produttore"=> "4"
+        ],
+        [
+            "id"=> "3",
+            "nome"=> "carezze",
+            "note"=>"",
+            "id_produttore"=> "5"
+        ],
+        [
+            "id"=> "4",
+            "nome"=> "cecca",
+            "note"=>"",
+            "id_produttore"=> "6"
+        ]
+    ]
+      
+];
